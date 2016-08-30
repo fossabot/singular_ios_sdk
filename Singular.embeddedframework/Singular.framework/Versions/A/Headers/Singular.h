b2c6d12ed7b195e92d9c16d308c4978c98aec1fb
Dima Pavlovsky <dima@singular.net>
2016-08-30 14:19:44 +0300

    release 1.3.3

--- Singular.embeddedframework/Singular.framework/Versions/A/Headers/Singular.h
+++ Singular.embeddedframework/Singular.framework/Versions/A/Headers/Singular.h
@@ -25,6 +25,7 @@
  */
 +(NSString *)singularId;
 
+
 /**
  * Tell Singular that a particular event has happened.
  *
@@ -32,7 +33,6 @@
  * @param eventProperties An optional NSDictionary containing the callback parameters.
  */
 +(void)trackEvent:(NSString *)eventType withProperties:(NSDictionary *)eventProperties;
-
 
 
 /**
@@ -61,33 +61,29 @@
 +(void)trackEvent:(NSString *)eventType withAttributes:(NSString *)firstAttribute, ... NS_REQUIRES_NIL_TERMINATION;
 
 
-
 /**
  * Tell Singular that a user generated some revenue.
  * Uses the default currency as the currency for this event
  *
- * @param price The price in cents (example: 1.5 means one and a half cents)
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
  */
 +(void)trackRevenue:(double)price;
 
 
-
 /**
  * Tell Singular that a user generated some revenue.
  *
- * @param price The price in cents (example: 1.5 means one and a half cents)
- * @param currency The currency to measure the revenue. While the currency is an NSString it should belong to the kSingularCurrency* constants 
- * available in SingularCurrency.h
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
  */
 +(void)trackRevenue:(double)price withCurrency:(NSString *)currency;
-
 
 
 /**
  * Tell Singular that a user generated some revenue.
  * Uses the default currency as the currency for this event
  *
- * @param price The price in cents (example: 1.5 means one and a half cents)
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
  * @param transaction The StoreKit transaction item from the purchase.
  */
 +(void)trackRevenue:(double)price withStoreKitTransaction:(SKPaymentTransaction *)transaction;
@@ -96,22 +92,20 @@
 /**
  * Tell Singular that a user generated some revenue.
  *
- * @param price The price in cents (example: 1.5 means one and a half cents)
- * @param currency The currency to measure the revenue. While the currency is an NSString it should belong to the kSingularCurrency* constants
- * available in SingularCurrency.h
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
  * @param transaction The StoreKit transaction item from the purchase. 
  */
 +(void)trackRevenue:(double)price withCurrency:(NSString *)currency withStoreKitTransaction:(SKPaymentTransaction *)transaction;
-
 
 
 /**
  * Tell Singular that a user generated some revenue.
  * Uses the default currency as the currency for this event
  *
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
  * @param productId An optional identifier differentiating between the different products available
  * @param quantity How many units were bought
- * @param price The price in cents (example: 1.5 means one and a half cents)
  */
 +(void)trackRevenue:(double)price withProductId:(NSString *)productId withQuantity:(int)quantity;
 
@@ -119,11 +113,10 @@
 /**
  * Tell Singular that a user generated some revenue.
  *
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
  * @param productId An optional identifier differentiating between the different products available
  * @param quantity How many units were bought
- * @param price The price in cents (example: 1.5 means one and a half cents)
- * @param currency The currency to measure the revenue in. While the currency is an NSString it should belong to the kSingularCurrency* constants
- * available in SingularCurrency.h
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
  */
 +(void)trackRevenue:(double)price withProductId:(NSString *)productId withQuantity:(int)quantity currency:(NSString *)currency;
 
@@ -131,14 +124,68 @@
 /**
  * Tell Singular that a user generated some revenue.
  *
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
  * @param productId An optional identifier differentiating between the different products available
  * @param quantity How many units were bought
- * @param price The price in cents (example: 1.5 means one and a half cents)
- * @param currency The currency to measure the revenue in. While the currency is an NSString it should belong to the kSingularCurrency* constants
- * available in SingularCurrency.h
+ * @param transaction The StoreKit transaction item from the purchase.
+ */
++(void)trackRevenue:(double)price withProductId:(NSString *)productId withQuantity:(int)quantity storeKitTransaction:(SKPaymentTransaction *)transaction;
+
+
+/**
+ * Tell Singular that a user generated some revenue.
+ *
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param productId An optional identifier differentiating between the different products available
+ * @param quantity How many units were bought
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
  * @param transaction The StoreKit transaction item from the purchase.
  */
 +(void)trackRevenue:(double)price withProductId:(NSString *)productId withQuantity:(int)quantity currency:(NSString *)currency storeKitTransaction:(SKPaymentTransaction *)transaction;
+
+
+/**
+ * Tell Singular that a user generated some revenue.
+ * Uses the default currency as the currency for this event
+ *
+ * @param base64Receipt The base64 encoded value of the receiptData in iOS 6 and lower, or the data that resides in the appStoreReceiptURL location in iOS 7 and up
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ */
++(void)trackRevenueWithReceipt:(NSString *)base64Receipt andPrice:(double)price;
+
+
+/**
+ * Tell Singular that a user generated some revenue.
+ *
+ * @param base64Receipt The base64 encoded value of the receiptData in iOS 6 and lower, or the data that resides in the appStoreReceiptURL location in iOS 7 and up
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
+ */
++(void)trackRevenueWithReceipt:(NSString *)base64Receipt withPrice:(double)price andCurrency:(NSString *)currency;
+
+
+/**
+ * Tell Singular that a user generated some revenue.
+ *
+ * @param base64Receipt The base64 encoded value of the receiptData in iOS 6 and lower, or the data that resides in the appStoreReceiptURL location in iOS 7 and up
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param productId An optional identifier differentiating between the different products available
+ * @param quantity How many units were bought
+ */
++(void)trackRevenueWithReceipt:(NSString *)base64Receipt withPrice:(double)price withProductId:(NSString *)productId andQuantity:(int)quantity;
+
+
+/**
+ * Tell Singular that a user generated some revenue.
+ *
+ * @param base64Receipt The base64 encoded value of the receiptData in iOS 6 and lower, or the data that resides in the appStoreReceiptURL location in iOS 7 and up
+ * @param price The price in units of your currency of choice (example: 1.5 means one and a half dollars (if the default currency is set to USD))
+ * @param productId An optional identifier differentiating between the different products available
+ * @param quantity How many units were bought
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
+ */
++(void)trackRevenueWithReceipt:(NSString *)base64Receipt withPrice:(double)price withProductId:(NSString *)productId withQuantity:(int)quantity andCurrency:(NSString *)currency;
+
 
 /**
  * Tracks app launches resulting from deep link. This method should be called from UIApplicationDelegate's application:openUrl and passed the same arguments.
@@ -147,6 +194,7 @@
  * @param annotation
  */
 +(void)trackOpenUrl:(NSURL*)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation;
+
 
 /**
  * Sets the Facebook id of the user
@@ -161,13 +209,11 @@
 +(void)unsetFacebookId;
 
 
-
 /**
  * Sets the Google id of the user
  * @param id a unique identifier received from Google
  */
 +(void)setGoogleId:(NSString *)googleId;
-
 
 
 /**
@@ -196,7 +242,6 @@
 +(void)setCustomUserId:(NSString *)userId;
 
 
-
 /**
  * Un-sets a previously set custom id
  */
@@ -211,19 +256,22 @@
 
 /**
  * Sets the default currency for future trackRevenue calls. The SDK default is kSingularCurrencyUSD
- * @param currency - The currency to measure the revenue in. While the currency is an NSString it should belong to the kSingularCurrency* constants.
+ * @param currency The currency to measure the revenue. A list of world currencies can be found in SingularCurrency.h
  */
 +(void)setDefaultCurrency:(NSString *)currency;
+
 
 /**
  * Gets the default currency
  */
 +(NSString *)defaultCurrency;
 
+
 /**
  * disables tracking for this device (for example, in case the user opts-out)
  */
 +(void)disableDeviceTracking;
+
 
 /**
  * enables tracking for this device
